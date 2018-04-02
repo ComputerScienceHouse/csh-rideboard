8e23f33e460de045d4987f64b9975b5e892cf7ed
axg4975 <axg4975@rit.edu>
2018-04-02 01:20:00 -0400

    changing port number

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+# Port 8080 for openshift
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
