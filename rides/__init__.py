93afae7e3496c77d18108c8f8e3109306af8c5ce
Devin Matte <devinmatte@gmail.com>
2018-04-02 14:15:54 -0400

    Adding reading the config

--- rides/__init__.py
+++ rides/__init__.py
@@ -1,6 +1,13 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Get app config from absolute file path
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
 
 @app.route('/')
 def hello_world():
