e0b17e7292b3392223fa27a95aad3d42f915cf4e
Fred Rybin <fredrybin@gmail.com>
2018-04-06 13:21:50 -0400

    Updated the database model to include when cars leave, come back, and any commects that the driver would like to add

--- rides/__init__.py
+++ rides/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
 # Get app config from absolute file path
 if os.path.exists(os.path.join(os.getcwd(), "config.py")):
     app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
@@ -13,6 +12,7 @@ else:
 
 db = SQLAlchemy(app)
 
+# pylint: disable=wrong-import-position
 from rides.models import Ride, Rider, Car
 
 @app.route('/')
--- rides/models.py
+++ rides/models.py
@@ -29,14 +29,21 @@ class Car(db.Model):
     name = db.Column(db.String(50), nullable=False)
     current_capacity = db.Column(db.Integer, nullable=False)
     max_capacity = db.Column(db.Integer, nullable=False)
+    departure_time = db.Column(db.DateTime, nullable=False)
+    return_time = db.Column(db.DateTime, nullable=False)
+    driver_comment = db.Column(db.Text)
     ride_id = db.Column(db.Integer, db.ForeignKey('rides.id'), nullable=False)
     riders = db.relationship('Rider', backref='cars', lazy=True)
 
-    def __init__(self, username, name, current_capacity, max_capacity, ride_id):
+    def __init__(self, username, name, current_capacity, max_capacity,
+         departure_time, return_time, driver_comment, ride_id):
         self.username = username
         self.name = name
         self.current_capacity = current_capacity
         self.max_capacity = max_capacity
+        self.departure_time = departure_time
+        self.return_time = return_time
+        self.driver_comment = driver_comment
         self.ride_id = ride_id
 
     def __repr__(self):
