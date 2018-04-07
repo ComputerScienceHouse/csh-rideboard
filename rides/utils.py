383febee0dd02cc4984a25d3e9ade1d850ea38e9
axg4975 <axg4975@rit.edu>
2018-04-06 21:41:21 -0400

    Merge branch 'master' of https://github.com/axg4975/rides
    # Conflicts:
    #	rides/__init__.py

---
+++ rides/utils.py
@@ -0,0 +1,23 @@
+# Credit to Liam Middlebrook and Ram Zallan
+# https://github.com/liam-middlebrook/gallery
+
+
+from functools import wraps
+
+from flask import session
+
+def user_auth(func):
+    @wraps(func)
+    def wrapped_function(*args, **kwargs):
+        uuid = str(session["userinfo"].get("sub", ""))
+        uid = str(session["userinfo"].get("preferred_username", ""))
+
+        auth_dict = {
+            "uuid": uuid,
+            "uid": uid
+        }
+        kwargs["auth_dict"] = auth_dict
+
+        return func(*args, **kwargs)
+
+    return wrapped_function
