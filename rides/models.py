a2981ea7df1dde6ff90805a2cb80b16ecf205b87
Fred Rybin <fredrybin@gmail.com>
2018-06-19 22:52:15 -0400

    Upped the DB Character Limit for Names of Rides

--- rides/models.py
+++ rides/models.py
@@ -8,7 +8,7 @@ class Ride(db.Model):
     __tablename__ = 'rides'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
     address = db.Column(db.Text, nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=False)
