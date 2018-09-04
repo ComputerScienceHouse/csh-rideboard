2053203587cfc112ee978650b7b03ebc650952d9
frybin <fredrybin@gmail.com>
2018-09-04 00:28:46 -0400

    Added check for freshman names

--- rides/utils.py
+++ rides/utils.py
@@ -6,13 +6,18 @@ from functools import wraps
 
 from flask import session
 
+INTRO_REALM = "https://sso.csh.rit.edu/auth/realms/intro"
+
 def user_auth(func):
     @wraps(func)
     def wrapped_function(*args, **kwargs):
-        last = str(session["userinfo"].get("family_name", ""))
-        first = str(session["userinfo"].get("given_name", ""))
         uid = str(session["userinfo"].get("preferred_username", ""))
-
+        if session["id_token"]["iss"] == INTRO_REALM:
+            last = "(Intro)"
+            first = uid
+        else:
+            last = str(session["userinfo"].get("family_name", ""))
+            first = str(session["userinfo"].get("given_name", ""))
         auth_dict = {
             "first": first,
             "last": last,
